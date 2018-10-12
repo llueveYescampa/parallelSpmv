6c87e66ff08284e587ee082062091eaff5aa0d56
Edgar F. Black <edgarfblack@hotmail.com>
2018-10-11 21:17:44 -0500

    separating send and recive

--- src/startCommunication/startCommunication.c
+++ src/startCommunication/startCommunication.c
@@ -18,13 +18,7 @@ void startComunication(real *x_ptr,
     MPI_Comm_rank(MPI_COMM_WORLD,&worldRank);
     MPI_Comm_size(MPI_COMM_WORLD,&worldSize);
     
-    for (int process=0,indx=0, s=0, r=0; process < worldSize; indx += recvCount[process++] ) {
-
-        // forming x_off_ptr array with contributions from each rank
-        if (recvCount[process] > 0) {
-            MPI_Irecv(&x_off_ptr[indx],recvCount[process],MPI_MY_REAL,process, 231,MPI_COMM_WORLD,&requestR[r++] );
-        } // end if //    
-
+    for (int process=0,indx=0, s=0; process < worldSize; indx += recvCount[process++] ) {
         // need to compress data to send inside compressedVec
         if (sendCount[process] > 0) {                   // need to send to process j
             for (int i=0;  i<sendCount[process]; ++i) {
@@ -33,4 +27,12 @@ void startComunication(real *x_ptr,
             MPI_Isend(compressedVec[process],sendCount[process],MPI_MY_REAL,process, 231,MPI_COMM_WORLD,&requestS[s++] );
         } // end if //
     } // end for //
+    
+    for (int process=0,indx=0, r=0; process < worldSize; indx += recvCount[process++] ) {
+        // forming x_off_ptr array with contributions from each rank
+        if (recvCount[process] > 0) {
+            MPI_Irecv(&x_off_ptr[indx],recvCount[process],MPI_MY_REAL,process, 231,MPI_COMM_WORLD,&requestR[r++] );
+        } // end if //    
+    } // end for //
+
 } // end of startComunication() //
